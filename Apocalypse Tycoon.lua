01333f3db1370ea0e4afb43e5d704c8a7413d5fa
MR2GAMING <81989402+Pro-Gaming6@users.noreply.github.com>
2023-03-29 17:41:46 +0200

    Delete Apocalypse Tycoon.lua

--- Apocalypse Tycoon.lua
+++
@@ -1,5 +0,0 @@
-local args = {
-    [1] = math.huge
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("RaidService"):WaitForChild("RF"):WaitForChild("GiveReward"):InvokeServer(unpack(args))
