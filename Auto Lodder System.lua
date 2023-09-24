49b283ac055face7f7f5cf5232f63d0cc8fc54d3
Mofex_ <81989402+Mofex1@users.noreply.github.com>
2023-09-24 22:22:44 +0300

    Update Auto Lodder System.lua3

--- Auto Lodder System.lua
+++ Auto Lodder System.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://pastebin.com/mhtX5xjv"))()
-loadstring(game:HttpGet("https://pastebin.com/mhtX5xjv2"))()
+loadstring(game:HttpGet("https://pastebin.com/mhtX5xjv3"))()
